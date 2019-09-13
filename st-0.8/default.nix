101a77b6aafca821defd7ebb1bc634766b3ad42a
resonyze <vector@resonyze.xyz>
2019-09-13 15:58:00 +0530

    should add configDotH variable

--- st-0.8/default.nix
+++ st-0.8/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   src = ./src/st-0.8.2.tar.gz;
   scrollbackPatch = ./src/st-scrollback-0.8.2.diff;
   #base16Patch = ./src/st-base16-0.8.2.diff;
-  #configDotH = ./src/config.h;
+  configDotH = ./src/config.h;
   #colorScheme = ./src/colors/base16-vector-dark.h;
   buildInputs= [ xorg.libX11 xlibs.libXft ncurses6 pkgconfig ];
 } 
